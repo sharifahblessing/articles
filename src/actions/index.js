074d67ace108cf6ea5a3d05ae0c4249e2e51182c
Semanda Joe <kirabojoe@gmail.com>
2019-05-22 12:16:24 +0300

    articles

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
+export function addArticle(payload) {
+  return { type: ADD_ARTICLE, payload };
+}
+
+// our new action creator. Will it work?
+export function getData() {
+  return fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(json => {
+      return { type: "DATA_LOADED", payload: json };
+    });
+}
